8f58376cbb04af9259e842e676364f6e8b6df8b4
Jean Matbagan <matbaganjean@gmail.com>
2024-12-18 13:25:08 +0800

    Add files via upload

---
+++ sortArray.js
@@ -0,0 +1,11 @@
+let numbers = [24, 65, 21, 5, 9, 32, 42, 80, 57];
+let names = ["Zenvo", "Erica", "Jordie", "Alicia", "Rendon"];
+
+let merge = numbers.concat(names);
+console.log(merge);
+
+let numrev = numbers.reverse();
+console.log(numrev);
+
+let nameSorted = names.sort();
+console.log(nameSorted);
